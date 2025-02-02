6f3e58786e221a8a134a3926a9deb0371b8dc0d3
pashakxpy1 <pasha.kxpay@gmail.com>
2025-02-02 08:11:13 +0000

    chal

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-# l-10
+myBill = float(input("What was the bill?: "))
+numberOfPeople = int(input("How many people?: "))
+ptips=float(input("what % of tip: "))
+answer = round(myBill / numberOfPeople,2)
+total = answer + answer * ptips/100
+print("You all owe", answer, "and totla=", total)
